333462e70d5e2d9991fb4c5f1d8da4400d411d8a
Ahmad Borham <ahmad.borham.dev@gmail.com>
2024-11-11 15:59:33 +0200

    addig a filter Item

---
+++ lib/screens/filters.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class FiltersScreen extends StatefulWidget {
+  const FiltersScreen({super.key});
+  @override
+  State<FiltersScreen> createState() {
+    return _FiltersScreen();
+  }
+}
+
+class _FiltersScreen extends State<FiltersScreen> {
+  var _glutenFreeFilterSet = false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Your Filter",
+          style: Theme.of(context).textTheme.titleLarge,
+        ),
+      ),
+      body: Column(
+        children: [
+          SwitchListTile(
+            value: _glutenFreeFilterSet,
+            onChanged: (isChecked) {
+              setState(() {
+                _glutenFreeFilterSet = isChecked;
+              });
+            },
+            title: Text(
+              'Glutein-Free',
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.onSurface,
+                  ),
+            ),
+            subtitle: Text(
+              'Only Glutein-Free Meals',
+              style: Theme.of(context).textTheme.titleMedium,
+            ),
+            activeColor: Theme.of(context).colorScheme.tertiary,
+            contentPadding: const EdgeInsets.only(left: 34, right: 22),
+          ),
+        ],
+      ),
+    );
+  }
+}
